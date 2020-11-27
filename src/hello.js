ddd3b8b4a73691d47830698a9abb07325c46609a
ryuji <forest.book.programmer@gmail.com>
2020-11-27 16:43:26 +0900

    kindle 73% 5.6 Todoを追加するフォームを実装

---
+++ src/hello.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+    render() {
+        return <div>Hello, React!</div>
+    }
+}
+export default Hello;
---
+++ src/test_data.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+const UserName = (props) => {return <div>{props.name}</div>}
+
+class TestData extends Component {
+    render(){
+        return <p>aaa</p>
+    }
+
+}
+export default TestData;
